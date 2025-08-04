627a8a9fabc3a54a8cbccae0bca06cf7880a4a0a
kevinvyn1 <botx300@gmail.com>
2025-08-04 11:50:26 +0700

    Delete sql directory

--- sql/policies.sql
+++
@@ -1,7 +0,0 @@
-create policy if not exists "whitelist_select_admins_only"
-on public.whitelist for select to authenticated
-using ( exists (select 1 from public.admins a where a.user_id = auth.uid()) );
-create policy if not exists "whitelist_write_admins_only"
-on public.whitelist for all to authenticated
-using ( exists (select 1 from public.admins a where a.user_id = auth.uid()) )
-with check ( exists (select 1 from public.admins a where a.user_id = auth.uid()) );
