dbe6969044fbaca774a6d04a546db3dc71de280e
Kevin Sam <botx300@gmail.com>
2025-09-27 20:48:18 +0700

    Add files via upload

--- protect.js
+++ protect.js
@@ -2,10 +2,6 @@ import { hasValidSession } from './auth/auth.js';
 
 (function(){
   if (!hasValidSession()) {
-    const here = window.location.pathname + window.location.search + window.location.hash;
-    const target = '/auth/login.html?next=' + encodeURIComponent(here);
-    if (window.location.pathname !== '/auth/login.html') {
-      window.location.replace(target);
-    }
+    window.location.replace('/index.html');
   }
 })();
