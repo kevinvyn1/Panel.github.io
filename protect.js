6ec549046b003a4f978b9717aecb38186f6c209b
Kevin Sam <botx300@gmail.com>
2025-09-27 21:13:02 +0700

    Add files via upload

--- protect.js
+++ protect.js
@@ -1,7 +1,8 @@
 import { hasValidSession } from './auth/auth.js';
-
 (function(){
+  // Only gatekeep when this file is loaded, which we only include on admin.html
   if (!hasValidSession()) {
+    // redirect using relative URL to avoid 404 on Pages subpaths
     window.location.replace('index.html');
   }
 })();
