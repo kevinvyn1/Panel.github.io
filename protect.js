c1a79c750ee4247396932e67ccb9adb3e7244db9
Kevin Sam <botx300@gmail.com>
2025-09-27 21:14:40 +0700

    Delete protect.js

--- protect.js
+++
@@ -1,8 +0,0 @@
-import { hasValidSession } from './auth/auth.js';
-(function(){
-  // Only gatekeep when this file is loaded, which we only include on admin.html
-  if (!hasValidSession()) {
-    // redirect using relative URL to avoid 404 on Pages subpaths
-    window.location.replace('index.html');
-  }
-})();
